af8e1f3c23511561fc02ebec0d4491f7b2310f62
vadimpopov1 <bonia07052006@icloud.com>
2026-05-05 11:23:32 +0700

    linux fix

--- linux.sh
+++ linux.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-g++ -O2 -march=armv8-a -fno-tree-vectorize -o out main.cpp
+g++ -O2 -march=armv8-a -mfpu=neon -fno-tree-vectorize \
+  -I imgui -I imgui/backends \
+  -DGL_SILENCE_DEPRECATION \
+  imgui/imgui.cpp imgui/imgui_draw.cpp imgui/imgui_tables.cpp imgui/imgui_widgets.cpp \
+  imgui/backends/imgui_impl_glfw.cpp imgui/backends/imgui_impl_opengl3.cpp \
+  main.cpp -o out \
+  -lglfw -lGL
+
 ./out
